96e2869332d03b4ef383201ba047becadcc1800f
Tran Duc Quy <tranducquylhp@gmail.com>
2021-03-02 20:19:29 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Observable, timer} from 'rxjs';
+import {map, take} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'untitled';
+  listBackgroundChoose = [];
+  private intervalId = 0;
+  message = '';
+  count = 5;
+  remainingTime = this.count;
+  backgroundShow = '';
+  backgroundChoose = '#000000';
+
+  constructor() {
+  }
+  onChange(): void {
+    this.listBackgroundChoose.push(this.backgroundChoose);
+    if (this.backgroundShow === '') this.backgroundShow = this.listBackgroundChoose[0];
+  }
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+
+  start() {
+    this.countDown();
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.count;
+    }
+  }
+  reset() {
+    this.clearTimer();
+    this.remainingTime = this.count;
+    this.message = `Click start button to start the Countdown`;
+  }
+  index = 0;
+  private countDown() {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime === 0) {
+        this.remainingTime = this.count;
+        if (this.index === this.listBackgroundChoose.length - 1) {
+          this.index = 0;
+        } else {
+          this.index++;
+        }
+        this.backgroundShow = this.listBackgroundChoose[this.index];
+
+      }
+    }, 1000);
+  }
 }
